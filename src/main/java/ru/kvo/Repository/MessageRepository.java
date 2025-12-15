91a77a21267e50d9d04f8d1c219f9764ec858c77
alexkv2000 <alexkv2000@mail.ru>
2025-12-15 12:01:49 +0300

    add parameter Update MessageRepository.java

--- src/main/java/ru/kvo/Repository/MessageRepository.java
+++ src/main/java/ru/kvo/Repository/MessageRepository.java
@@ -20,7 +20,7 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
 
     @Modifying
     @Transactional
-    @Query("UPDATE Message m SET m.dateEnd = null, m.server = '' " +
+    @Query("UPDATE Message m SET  m.status = null, m.dateEnd = null, m.server = '', m.numAttempt = 0" +
             "WHERE m.id IN :ids")
     int resetMessages(@Param("ids") List<Long> ids);
 
